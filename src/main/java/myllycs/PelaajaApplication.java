cbee050214178543e09ef8e40abb8274c475d444
EetuLeppanen <64865883+EetuLeppanen@users.noreply.github.com>
2020-11-18 18:46:14 +0200

    Update PelaajaApplication.java

--- src/main/java/myllycs/PelaajaApplication.java
+++ src/main/java/myllycs/PelaajaApplication.java
@@ -34,7 +34,7 @@ public class PelaajaApplication {
 		
 		
 		
-		log.info("save two users");
+		log.info("tallennetaan admin ja user");
         User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
         User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
         urepository.deleteAll();
@@ -42,7 +42,7 @@ public class PelaajaApplication {
         urepository.save(user2);
         
         
-		log.info("save a couple players");
+		log.info("tallennetaan muuttama pelaaja sekä rooli");
 		rorepository.deleteAll();
 		rorepository.save(new Rooli("In-Game Leader"));
 		rorepository.save(new Rooli("Rifler"));
@@ -55,7 +55,7 @@ public class PelaajaApplication {
 		prepository.save(new Pelaaja("Eetu Leppänen", "1999", 1500000, rorepository.findByName("In-Game Leader").get(0)));
 		
 		
-		log.info("fetch all players");
+		log.info("fetchataan kaikki pelaajat");
 		for (Pelaaja pelaaja : prepository.findAll()) {
 			log.info(pelaaja.toString());
 		}
